6f72afd49c030cd41134f7d99daaa05254a2069e
Sebastian Weiß <sw@3ele.de>
2021-12-02 17:19:25 +0100

    init

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+
+FROM node:12
+
+WORKDIR /tmp
+RUN apt update && apt install -y lsof libxinerama1 libfontconfig1 libdbus-glib-1-2 libcairo2 libcups2 libglu1-mesa libsm6 unzip
+# Download LibreOffice
+RUN wget https://downloadarchive.documentfoundation.org/libreoffice/old/7.1.4.1/deb/x86_64/LibreOffice_7.1.4.1_Linux_x86-64_deb.tar.gz -O libo.tar.gz \
+    && tar -zxvf libo.tar.gz
+# Install LibreOffice
+WORKDIR /tmp/LibreOffice_7.1.4.1_Linux_x86-64_deb/DEBS
+RUN dpkg -i *.deb
+
+WORKDIR /home/node
+RUN npm install
+COPY index.js .
+COPY package.json .
+RUN mkdir output
+
+RUN mkdir templates
+#COPY templates ./templates/
+
+
+ENTRYPOINT ["node","index.js"]
+
---
+++ index.js
@@ -0,0 +1,33 @@
+  const fs = require('fs');
+  const carbone = require('carbone');
+  const path = require(`path`);
+
+  const util = require(`util`);
+
+  const express = require(`express`);
+  const bodyParser = require(`body-parser`);
+  const app = express();
+  const port = process.env.CARBONE_PORT || 3030;
+
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.post('/render', function(request, response) {
+      const data = request.body.data
+      const template = request.body.template
+      const options = request.body.options
+      const reportname = request.body.reportname
+      try {
+          carbone.render(template, data, options, function(err, result) {
+              if (err) return console.log(err);
+              fs.writeFileSync(path + 'output/' + reportname, result);
+          });
+
+      } catch (err) {
+          console.error(err)
+      }
+      response.send(reportname);
+  })
+
+  app.listen(port, () => console.log(`Carbone wrapper listenning on port ${port}!`));
